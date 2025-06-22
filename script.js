4ac275884db294c5f2563721ef03686350bf2c11
Bayanda Jack <2669074@students.wits.ac.za>
2025-06-22 20:07:38 +0200

    onClick functionality on the buttons to change textContent

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Yo
+document.addEventListener('DOMContentLoaded', () => {
+    //declaring our variables
+    const buttons = document.querySelectorAll(".btn");
+
+    //main functionality stuff
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            button.textContent = "X";
+        })
+    })
+})
