279fc64b55929c07c4422af9aef67887a03fe3ad
Bayanda Jack <2669074@students.wits.ac.za>
2025-06-22 20:48:25 +0200

    Added player O's functionality, so X and O show up with a space letting you know whose turn it is

--- script.js
+++ script.js
@@ -2,14 +2,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     //declaring our variables
     const buttons = document.querySelectorAll(".btn");
+    const turnHeader = document.querySelector(".turns");
 
     //main functionality stuff
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            if (button.textContent === "X" || button.textContent === "O"){
-                alert("A player has already played there!");
-            } else{
-                button.textContent = "X";
+            if (turnHeader.textContent === "Player X's turn"){
+                if (button.textContent === "X" || button.textContent === "O"){
+                    alert("A player has already played there!");
+                } else{
+                    button.textContent = "X";
+                    turnHeader.textContent = "Player O's turn";
+                }  
+            }else {
+                if (button.textContent === "X" || button.textContent === "O"){
+                    alert("A player has already played there!");
+                } else{
+                    button.textContent = "O";
+                    turnHeader.textContent = "Player X's turn";
+                }  
             }
         })
     })
