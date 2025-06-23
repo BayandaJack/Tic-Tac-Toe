f2f359236a02c0110c1c6505068e0e136b5a37fe
Bayanda Jack <2669074@students.wits.ac.za>
2025-06-23 12:25:20 +0200

    Added a board 2D array to keep track of the board so I can check the win status

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     //declaring our variables
     const buttons = document.querySelectorAll(".btn");
     const turnHeader = document.querySelector(".turns");
+    const board = [
+        ["", "", ""], // row 0 → (0,0), (0,1), (0,2)
+        ["", "", ""], // row 1 → (1,0), (1,1), (1,2)
+        ["", "", ""]  // row 2 → (2,0), (2,1), (2,2)
+    ];
 
     //main functionality stuff
     buttons.forEach(button => {
@@ -11,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (button.textContent === "X" || button.textContent === "O"){
                     alert("A player has already played there!");
                 } else{
+                    const id = button.id; //gets the cell so we can fill it in on the board
+                    board[id[0]][id[2]] = "X";
                     button.textContent = "X";
                     turnHeader.textContent = "Player O's turn";
                 }  
@@ -18,6 +25,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (button.textContent === "X" || button.textContent === "O"){
                     alert("A player has already played there!");
                 } else{
+                    const id = button.id; //gets the cell so we can fill it in on the board
+                    board[id[0]][id[2]] = "X";
                     button.textContent = "O";
                     turnHeader.textContent = "Player X's turn";
                 }  
